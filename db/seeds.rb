72add545fd64f60a68067522921b813df4d3bf27
Adam Foosaner <nucat176@gmail.com>
2017-08-20 09:15:04 -0700

    Adds some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: "a", tag: "a", is_guest?: true, password: "password"
+User.create(email: "b", tag: "b", is_guest?: true, password: "password"
+User.create(email: "c", tag: "c", is_guest?: true, password: "password"
+User.create(email: "d", tag: "d", is_guest?: true, password: "password"
+User.create(email: "e", tag: "e", is_guest?: true, password: "password"
+User.create(email: "f", tag: "f", is_guest?: true, password: "password"
+User.create(email: "g", tag: "g", is_guest?: true, password: "password"
+User.create(email: "h", tag: "h", is_guest?: true, password: "password"
+
+Tournament.create(creator_id: 1, tournament_name: "Andross Cup", date_start: Time.now, date_end: Time.now, is_published?: true, :is_finished?: true, has_started?: true)
+Tournament.create(creator_id: 2, tournament_name: "Laiqi Cup", date_start: Time.now, date_end: Time.now, is_published?: true, :is_finished?: true, has_started?: true)
